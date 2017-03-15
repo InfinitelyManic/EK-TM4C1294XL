300de33e99748d35a2a5a78f5b2b528f342687a0
David <agentdaffaires@gmail.com>
2017-03-15 13:57:56 -0700

    Update uart_startup_gnu.s

--- uart_startup_gnu.s
+++ uart_startup_gnu.s
@@ -8,7 +8,7 @@
 /*
         arm-none-eabi-as -g uart_startup_gnu.s -o uart_startup_gnu.o && arm-none-eabi-ld uart_startup_gnu.o -o uart_startup_gnu -T startup_gnu.ld                                            
 */
-//
+//  Last revision: 03/09/2017 @ 15:32 hours
 .syntax unified
 .thumb
 .bss
@@ -28,18 +28,20 @@
         .equ UARTn_BASE,                0x4000C000      // UART 0 Base
         .equ GPIO_PORTx_Base,           0x40058000      // Port x Base
 
-        .equ GPIO_PORTx_AHB_AFSEL_R,    0x420           // Alternate Func Select
-        .equ GPIO_PORTx_AHB_DEN_R,      0x51C           // Digital Enable
-        .equ GPIO_PORTx_AHB_AMSEL_R,    0x528           // Analogue Mode Select
-        .equ GPIO_PORTx_AHB_PCTL_R,     0x52C           // Port Ctrl
-        .equ GPIO_PORTx_AHB_DIR_R,      0x400           // GPIO Direction
+        .equ GPIO_PORT_F_Base,          0x4005D000      // Port F Base
+        .equ GPIO_PORT_N_Base,          0x40064000      // Port N Base
 
+        .equ GPIO_PORTx_AHB_DIR_R,      0x400           // GPIO Direction
+        .equ GPIO_PORTx_AHB_AFSEL_R,    0x420           // Alternate Func Select
+        .equ GPIOPUR,                   0x510           // GPIO Pull Up
+        .equ GPIO_PORTx_AHB_DEN_R,      0x51C           // Digital Enable
         .equ GPIOLOCK,                  0x520           // GPIO Lock  -
         .equ GPIOCR,                    0x524           // GPIO Commit
-        .equ GPIOPUR,                   0x510           // GPIO Pull Up
+        .equ GPIO_PORTx_AHB_AMSEL_R,    0x528           // Analogue Mode Select
+        .equ GPIO_PORTx_AHB_PCTL_R,     0x52C           // Port Ctrl
 
-        .equ UARTn_RSR_R,               0x004           // Receive Status/Error
         .equ UARTn_DR_R,                0x000           // Data
+        .equ UARTn_RSR_R,               0x004           // Receive Status/Error
         .equ UARTn_FR_R,                0x018           // Flag
         .equ UARTn_IBRD_R,              0x024           // Int Baud Rate Divr
         .equ UARTn_FBRD_R,              0x028           // Frac Baud Rate Divr
@@ -65,7 +67,6 @@ Vecs:
         .word IntDefaultHandler         // bus fault handler
         .word IntDefaultHandler         // usage fault handler
 
-
 // UART init for UART
 .thumb_func
 .global init_UART
@@ -83,7 +84,6 @@ init_UART:
         and r12, r12, #(1 << 0)         // mask GPIO ready bit for UART n
         cmp r12, #0
         beq 1b                          // loop until ready bit is set
-
         // config UARTn
         ldr r1,=UARTn_BASE              // UARTn Base
 
@@ -95,6 +95,8 @@ init_UART:
         ldr r2,=UARTn_CC_R
         mov r12, #0x0                   // is not 0 then us clock
         str r12, [r1, r2]               // config clock source - default is 00
+
+        ldr r2,=UARTn_IBRD_R
         movw r12, #8                    // IBRD=int(16MHz/(16*115,200))=int()=8
         str r12, [r1, r2]
 
@@ -112,7 +114,6 @@ init_UART:
         bx lr
         // config UART done *********************************************************
 .align
-
 
 
 // GPIO init for UART
@@ -158,20 +159,99 @@ init_GPIO_4_UART:
         bx lr
         // config GPIO_4_UART ************** done *******************************
 .align
+.thumb_func
+.global init_GPIO_4_LED_F
+init_GPIO_4_LED_F:
+        // GPIO init for LED Port F
+        ldr r1,=SCS_BASE                // System Control Base
+
+        ldr r2,=RCGCGPIO
+        ldr r12, [r1,r2]
+        orr r12, r12, #(1 << 5)         // Enable clock re GPIO Port x
+        str r12,[r1, r2]
+        //wait for clock to be ready
+        ldr r2,=PRGPIO                  // GPIO Peripheral Ready
+1:
+        ldrh r12, [r1,r2]               // get state - 16 bits
+        and r12, r12, #(1 << 5)         // get GPIO ready bit for Port x
+        cmp r12, #0
+        beq 1b                          // loop until ready bit is set
+        // config GPIO Port F
+        ldr r1,=GPIO_PORT_F_Base        // GPIO Port F AHB:w
+
+        ldr r2,=GPIO_PORTx_AHB_DIR_R    // ********
+        ldr r12, [r1,r2]
+        orr r12, r12, #0x11             // 0001|00001 PF4, PF0
+        str r12, [r1,r2]                // set pins to output
+
+        ldr r2,=GPIO_PORTx_AHB_DEN_R
+        ldr r12, [r1,r2]
+        orr r12, r12, #0x11             // enable all .... PF4, PF0
+        str r12, [r1,r2]                // set pins to enable digital function
+        bx lr
+.align
+.thumb_func
+.global init_GPIO_4_LED_N
+init_GPIO_4_LED_N:
+        // GPIO init for LED Port N
+        ldr r1,=SCS_BASE                // System Control Base
+
+        ldr r2,=RCGCGPIO
+        ldr r12, [r1, r2]
+        orr r12, r12, #(1 << 12)        // Enable clock re GPIO Port x
+        str r12,[r1, r2]
+        //wait for clock to be ready
+        ldr r2,=PRGPIO                  // GPIO Peripheral Ready
+1:
+        ldrh r12, [r1,r2]               // get state - 16 bits
+        and r12, r12, #(1 << 12)        // get GPIO ready bit for Port x
+        cmp r12, #0
+        beq 1b                          // loop until ready bit is set
+        // config GPIO Port N
+        ldr r1,=GPIO_PORT_N_Base        // GPIO Port N AHB
+
+        ldr r2,=GPIO_PORTx_AHB_DIR_R    //
+        ldr r12, [r1,r2]
+        orr r12, r12, #0x03             // 0000|0011 PN1, PN0
+        str r12, [r1,r2]                // set pins to output
+
+        ldr r2,=GPIO_PORTx_AHB_DEN_R
+        ldr r12, [r1,r2]
+        orr r12, r12, #0x03             // PN1, PN0
+        str r12, [r1,r2]                // set pins to enable digital function
+        bx lr
+.align
+.thumb_func
+.global PortF
+PortF:
+        ldr r1,=GPIO_PORT_F_Base                // GPIO Port F AHB
+        add r1, r1, #0x0044                     // bit-specific addressing
+        and r0, r0, #0b00010001
+        str r0, [r1]
+        bx lr
+.align
+
+.thumb_func
+.global PortN
+PortN:
+        ldr r1,=GPIO_PORT_N_Base                // GPIO Port N AHB
+        add r1, r1, #0x000c                     // bit-specific addressing
+        and r0, r0 ,#0b00000011
+        str r0, [r1]
+        bx lr
+.align
 
 
 .thumb_func
 _delay:
         movw r12, #0xffff
-        movt r12, #0x1
+        movt r12, #0x7
 1:
         nop
         subs r12, r12, #1
         bpl 1b
         bx lr
 .align
-
-
 .thumb_func
 uart_receive:
         ldr r1,=UARTn_BASE              // UARTn Base
@@ -205,16 +285,18 @@ uart_transmit:
 .thumb_func
 .global Start
 Start:
-        nop
         bl init_UART                    // init GPIO
         bl init_GPIO_4_UART             // init GPIO for UART
+        bl init_GPIO_4_LED_F            // init GPIO for LED F
+        bl init_GPIO_4_LED_N            // init GPIO for LED N
 .stop0:
+        nop
         // Linear congruential generator s/u
         ldr r12,=M
         mov r0, #2                      // init seed
         str r0, [r12]                   // M=1
 .loop:
-        bl Random
+        bl _random
 
         rbit r0, r0                     // makes the output appear more random
         and r0, r0, #0xff               // mask
@@ -224,10 +306,19 @@ Start:
         bgt .skip
 
         bl uart_transmit                // print to UART
+
+        // blink leds
+        mov r12, r0                     // save copy of r0
+//      and r0, r0, #0b00000011         // get first two bits 00xx
+        bl PortN                        // toggle PN0 & PN1
+        mov r0, r12                     // get copy of r0
+//      and r0, r0, #0b00001100         // get bits 2 & 3
+        bl PortF                        // toggle PF0 & PF4
+
         bl _delay                       // keeps the load average when viewing output
 .skip:
-        bl .loop
-Random:                 //Linear congruential generator
+        b .loop
+_random:                        //Linear congruential generator
         ldr r12,=M
         ldr r0,[r12]
         ldr r1,=1664525
@@ -237,6 +328,7 @@ Random:                 //Linear congruential generator
         str r0,[r12]
         bx lr
 .align
+
 .text
 // ************ NVIC interrupts **************
 .global _start
@@ -262,3 +354,5 @@ FaultISR:                               // Hard fault
 IntDefaultHandler:                      // IntDefault handler
         b .
 .align
+
+.end
